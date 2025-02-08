dd5b8f988947a46d24c23a7958e6a1537eed1bb9
apinanlamt <apple8511id@gmail.com>
2025-02-09 03:22:52 +0700

    updateGetarea

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -4,6 +4,7 @@ const roomRoute = express.Router();
 
 roomRoute.get("/", roomController.getAllRooms);
 roomRoute.get("/:id", roomController.getRoomById);
+roomRoute.get('/area/:area', roomController.getRoomsByArea); //(CB2, LX, SIT)
 roomRoute.post("/", roomController.createRoom);
 roomRoute.put("/:id", roomController.updateRoom);
 roomRoute.delete("/:id", roomController.deleteRoom);
